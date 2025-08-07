32bd10659b1de14a3a18783d60eae944f0264166
Darwin <jackdarwinbot@gmail.com>
2025-08-07 17:40:52 -0600

    tercer comit,se termnino la opcion 1 y 2, problemas para usar diccionario

--- Actividad13Ordenamiento.py
+++ Actividad13Ordenamiento.py
@@ -6,11 +6,12 @@ class Repartidor:
 
     def __str__(self):
         return (f"{self.nombre}, {self.paquetes},{self.zona}")
+
 class Mensajeria:
     def __init__(self):
-        self.repartidores=[]
+        self.repartidores={}
     def agregarRepartidores(self,repartidor):
-        self.repartidores.append(repartidor)
+        self.repartidores[repartidor.nombre]=repartidor
 
     def quick_sort(lista):
         if len(lista) <= 1:
@@ -23,15 +24,15 @@ class Mensajeria:
 
         return quick_sort(menores) + iguales + quick_sort(mayores)
 
-
-
+empresa=Mensajeria()
 while True:
     print("Menu")
     print("1. Ingresar")
     print("2. Mostar")
     print("3. Ordenar")
-    print("4. Estadisticas")
-    print("5. Salir")
+    print("4 .Buscar")
+    print("5. Estadisticas")
+    print("6. Salir")
     try:
         opcion=int(input("Ingrese una opcion"))
     except ValueError:
@@ -39,13 +40,29 @@ while True:
     match opcion:
         case 1:
             print("Ingreso Repartidores")
+            nombre = input("Nombre del Repartidor: ")
+            try:
+                paquetes = int(input("Cantidad de paquetes entregados: "))
+            except ValueError:
+                print("Ingrese un entero.")
+                continue
+            zona = input("Zona: ")
+            empresa.agregarRepartidores(Repartidor(nombre, paquetes, zona))
         case 2:
             print("Mostrar ")
+            if not empresa.repartidores:
+                print("No hay repartidores registrados.")
+            else:
+                print("Lisatodo original")
+                for r in empresa.repartidores.values():
+                    print(r)
         case 3:
             print("Ordenarlos")
         case 4:
-            print("Estadisticas")
+            print("Buscar")
         case 5:
+            print("Estadisticas")
+        case 6:
             print("Salir")
             break
         case _:
