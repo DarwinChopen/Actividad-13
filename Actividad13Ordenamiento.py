4f71666f5c136d6b99b0be13dd2ca7ac853bedc3
Darwin <jackdarwinbot@gmail.com>
2025-08-07 16:45:54 -0600

    Segundo comit,

--- Actividad13Ordenamiento.py
+++ Actividad13Ordenamiento.py
@@ -1,3 +1,30 @@
+class Repartidor:
+    def __init__(self, nombre,paquetes,zona):
+        self.nombre=nombre
+        self.paquetes=paquetes
+        self.zona=zona
+
+    def __str__(self):
+        return (f"{self.nombre}, {self.paquetes},{self.zona}")
+class Mensajeria:
+    def __init__(self):
+        self.repartidores=[]
+    def agregarRepartidores(self,repartidor):
+        self.repartidores.append(repartidor)
+
+    def quick_sort(lista):
+        if len(lista) <= 1:
+            return lista
+
+        pivote = lista[0]
+        menores = [x for x in lista[1:] if x < pivote]
+        iguales = [x for x in lista if x == pivote]
+        mayores = [x for x in lista[1:] if x > pivote]
+
+        return quick_sort(menores) + iguales + quick_sort(mayores)
+
+
+
 while True:
     print("Menu")
     print("1. Ingresar")
